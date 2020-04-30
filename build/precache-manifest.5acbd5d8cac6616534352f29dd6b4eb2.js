5a4bdb2a2f6b56d8b995fc556007e4075551717d
Varis <varis@Variss-MacBook-Pro.local>
2020-04-30 12:18:56 +0300

    initial

---
+++ build/precache-manifest.5acbd5d8cac6616534352f29dd6b4eb2.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ed114d60b85a821e76409eff1e9635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04d5043ba1b0b7fae9c",
+    "url": "/static/css/2.cbf4de94.chunk.css"
+  },
+  {
+    "revision": "7390917c6495ce6bc5f7",
+    "url": "/static/css/main.fb847f1f.chunk.css"
+  },
+  {
+    "revision": "d04d5043ba1b0b7fae9c",
+    "url": "/static/js/2.24bedafe.chunk.js"
+  },
+  {
+    "revision": "5f2dfc729cbc5e03eb64677a2da4dad4",
+    "url": "/static/js/2.24bedafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7390917c6495ce6bc5f7",
+    "url": "/static/js/main.89e93bd9.chunk.js"
+  },
+  {
+    "revision": "fa7b44e73105e2159e0d",
+    "url": "/static/js/runtime-main.ed5d6d56.js"
+  },
+  {
+    "revision": "4b0840df527f6dd303ecfe418a2a378c",
+    "url": "/static/media/0.Logo-Marvel.4b0840df.svg"
+  },
+  {
+    "revision": "b498b02a963ac763c1a64c4b51dc7945",
+    "url": "/static/media/1.Hero-img4.b498b02a.png"
+  },
+  {
+    "revision": "f121490f436994ff36919e0ac0199b33",
+    "url": "/static/media/1.POWERFUL-SECURITY.f121490f.svg"
+  },
+  {
+    "revision": "b1a239c40b48454bd1bf8b90c444a4f2",
+    "url": "/static/media/10.Action-Bg.b1a239c4.png"
+  },
+  {
+    "revision": "7ea076dc41261418716c538b44619992",
+    "url": "/static/media/11.Blog_Post-1.7ea076dc.png"
+  },
+  {
+    "revision": "21741a3409c8191c1f490c53eb722418",
+    "url": "/static/media/11.Blog_Post-2.21741a34.png"
+  },
+  {
+    "revision": "a578cf97b188167ce15d83a4f738046a",
+    "url": "/static/media/11.Blog_Post-4.a578cf97.png"
+  },
+  {
+    "revision": "780e68df915546beb36738d1c7615b64",
+    "url": "/static/media/12.ContactUs_Send-Bg.780e68df.png"
+  },
+  {
+    "revision": "30754d661f893f3cbbd044648711424f",
+    "url": "/static/media/13.Location.30754d66.png"
+  },
+  {
+    "revision": "6c302a0b674e7203bf26bb4f1fd35e83",
+    "url": "/static/media/2.About-Awesome_Features.6c302a0b.svg"
+  },
+  {
+    "revision": "25b877c4db6052187f044927a6d43b0c",
+    "url": "/static/media/2.About-Powerful_Security.25b877c4.svg"
+  },
+  {
+    "revision": "82dc46122adb26dbee5cab6dcf6d8364",
+    "url": "/static/media/2.About-Responsive_Design.82dc4612.svg"
+  },
+  {
+    "revision": "a2c330c46db4b9338c4967ae319163da",
+    "url": "/static/media/2.About-ipad.a2c330c4.png"
+  },
+  {
+    "revision": "0a5dbf11a5b0e6970361b698b3ccd21a",
+    "url": "/static/media/2.CREATIVE-IDEAS.0a5dbf11.svg"
+  },
+  {
+    "revision": "1209138fdf914cba69b2030602c0449a",
+    "url": "/static/media/3.POWERFULL-ADMIN-PANEL.1209138f.svg"
+  },
+  {
+    "revision": "7a3c03a13849da9257446a6d6df34ce3",
+    "url": "/static/media/3.Responsive-Run.7a3c03a1.png"
+  },
+  {
+    "revision": "9260b7dfbc70d08c518c071fd93934b2",
+    "url": "/static/media/3.Responsive-lamp.9260b7df.png"
+  },
+  {
+    "revision": "36b30a88a7f096fa4fe61bfa4f3e46aa",
+    "url": "/static/media/4.HIGH-RESOLUTION-THEME.36b30a88.svg"
+  },
+  {
+    "revision": "f5efd642f33ddc73372bdff0f9010247",
+    "url": "/static/media/4.WhoWeAre_Bg.f5efd642.png"
+  },
+  {
+    "revision": "afd2836279a5bfdc7107e13b15e7d691",
+    "url": "/static/media/5.AWESOME-INTERFACE.afd28362.svg"
+  },
+  {
+    "revision": "a3048ef24124c1df7a3101ed6e83bbe7",
+    "url": "/static/media/6.Gallery_Img-1.a3048ef2.png"
+  },
+  {
+    "revision": "8677a90a1c4ad5de458c51f6f6f65d48",
+    "url": "/static/media/6.Gallery_Img-2.8677a90a.png"
+  },
+  {
+    "revision": "abfa7fbab13e619e5d659ee3f7c32783",
+    "url": "/static/media/6.Gallery_Img-3.abfa7fba.png"
+  },
+  {
+    "revision": "f5ee98edfd6128777c557013472392fc",
+    "url": "/static/media/6.Gallery_Img-4.f5ee98ed.png"
+  },
+  {
+    "revision": "c995449b09354f170eef60701e9559da",
+    "url": "/static/media/6.Gallery_Img-5.c995449b.png"
+  },
+  {
+    "revision": "786d92df3eec515896739c4662397708",
+    "url": "/static/media/6.Gallery_Img-6.786d92df.png"
+  },
+  {
+    "revision": "05bb6212a7de871f1ac76c0ad8634ca0",
+    "url": "/static/media/6.Gallery_Img-7.05bb6212.png"
+  },
+  {
+    "revision": "4cea45a66c70a1b446abd0765558243a",
+    "url": "/static/media/6.Gallery_Img-8.4cea45a6.png"
+  },
+  {
+    "revision": "3a5be28697a324e0c95aaf9f8224cb3f",
+    "url": "/static/media/6.UNTIMADE-WEBSITE-BUILDER.3a5be286.svg"
+  },
+  {
+    "revision": "e0214e1bcd9c2f2cd11ddcaff8995513",
+    "url": "/static/media/7.RESPONSIVE-THEME.e0214e1b.svg"
+  },
+  {
+    "revision": "3e0c6431abe604cf167f0b20b8c4ea6b",
+    "url": "/static/media/7.Service_Img.3e0c6431.png"
+  },
+  {
+    "revision": "59152a961234c7b977a51e20fa673e0f",
+    "url": "/static/media/8.CUSTOM-CONTENT-BLOCKS.59152a96.svg"
+  },
+  {
+    "revision": "7a70a762a9c413bad9237305f5887d3c",
+    "url": "/static/media/8.Price-Bg.7a70a762.png"
+  },
+  {
+    "revision": "23044bf117d4ffc73847b1cf87146bdd",
+    "url": "/static/media/8.Price-Card1.23044bf1.png"
+  },
+  {
+    "revision": "ed6b706fe60634822325b757823ff52e",
+    "url": "/static/media/8.Price-Card2.ed6b706f.png"
+  },
+  {
+    "revision": "4f2c020d4a802772cabdccf00d3f1eba",
+    "url": "/static/media/8.Price-Card3.4f2c020d.png"
+  },
+  {
+    "revision": "c494efd3310ba1dd62a9499f6c612e19",
+    "url": "/static/media/8.branding1.c494efd3.jpg"
+  },
+  {
+    "revision": "02d5ff9c85fef3be17c4291bf29e89e4",
+    "url": "/static/media/8.branding2.02d5ff9c.jpg"
+  },
+  {
+    "revision": "6c2dc5373317bc088942553dafd1d6ef",
+    "url": "/static/media/8.business1.6c2dc537.jpg"
+  },
+  {
+    "revision": "ab45a464da207cf06da2e7806b167379",
+    "url": "/static/media/8.business2.ab45a464.jpg"
+  },
+  {
+    "revision": "ecca0ac75580c4a230d1b0a001c4c3f4",
+    "url": "/static/media/8.corporate1.ecca0ac7.jpg"
+  },
+  {
+    "revision": "b3d8a985a4e59288265a4549df3cb653",
+    "url": "/static/media/8.corporate2.b3d8a985.jpg"
+  },
+  {
+    "revision": "716f3830a28c94e6989ddae9f64597c4",
+    "url": "/static/media/9.TRENDY-DESIGN.716f3830.svg"
+  },
+  {
+    "revision": "bcd4bc13df64716d2b7ae68c74ec815a",
+    "url": "/static/media/9.Team_Img-1.bcd4bc13.png"
+  },
+  {
+    "revision": "03a7ee6a96efbb0a89b3b2ce0174f5ed",
+    "url": "/static/media/9.Team_Img-2.03a7ee6a.png"
+  },
+  {
+    "revision": "e4f4e0edff3a3b0d30b56624e263b56c",
+    "url": "/static/media/9.Team_Img-3.e4f4e0ed.png"
+  },
+  {
+    "revision": "edb47f3abeec9723b85e333744cd4414",
+    "url": "/static/media/All-Button-Icon.edb47f3a.svg"
+  },
+  {
+    "revision": "75a765e0e0c8f6dfcd68858c82eebe75",
+    "url": "/static/media/ArrowNext.75a765e0.svg"
+  },
+  {
+    "revision": "964f6d79022b5e7f42872fd8171faf45",
+    "url": "/static/media/ArrowPrev.964f6d79.svg"
+  },
+  {
+    "revision": "988d3f3db3fce879a2c7c80b9cd37dee",
+    "url": "/static/media/Bicycle-Logo.988d3f3d.svg"
+  },
+  {
+    "revision": "d7052bf381b2c27410d360df76a82fbe",
+    "url": "/static/media/GlobeIcon.d7052bf3.svg"
+  },
+  {
+    "revision": "658da981f1fc26a7f1d21c298f06af4d",
+    "url": "/static/media/Link.658da981.svg"
+  },
+  {
+    "revision": "85d81587524bb0db5184dfe4a9f8dbb0",
+    "url": "/static/media/Locate-Icon.85d81587.svg"
+  },
+  {
+    "revision": "1ab65caebc17865365e45f5e774d774b",
+    "url": "/static/media/MaterialIcons-Regular.1ab65cae.woff"
+  },
+  {
+    "revision": "3febe99def633724384d119b31524874",
+    "url": "/static/media/MaterialIcons-Regular.3febe99d.woff2"
+  },
+  {
+    "revision": "7be74d583acd2d20383222f4d157880e",
+    "url": "/static/media/MaterialIcons-Regular.7be74d58.ttf"
+  },
+  {
+    "revision": "969d5d9a4c903a2726194b487356bcd1",
+    "url": "/static/media/PhoneIcon.969d5d9a.svg"
+  },
+  {
+    "revision": "fda76404427c0321e854c01336a06085",
+    "url": "/static/media/View.fda76404.svg"
+  },
+  {
+    "revision": "7c12c804ab01cba1b5ac702a7d2ec4de",
+    "url": "/static/media/about1.7c12c804.svg"
+  },
+  {
+    "revision": "b8e2111d85c9f894acd963028e68635a",
+    "url": "/static/media/appleIcon.b8e2111d.svg"
+  },
+  {
+    "revision": "2a1e2e325ef2cc292be793c3cf544caa",
+    "url": "/static/media/close.2a1e2e32.svg"
+  },
+  {
+    "revision": "f4bf597ce81a372653647ce888ecba29",
+    "url": "/static/media/excellentFeatures.f4bf597c.svg"
+  },
+  {
+    "revision": "c05aa2189dc71ce07a0ab7448e9c94de",
+    "url": "/static/media/fasebookIcon.c05aa218.svg"
+  },
+  {
+    "revision": "0d6945b535fe49a1addc847f238f97b1",
+    "url": "/static/media/home1.0d6945b5.svg"
+  },
+  {
+    "revision": "ee606320a412a56d149846e2495cf6bc",
+    "url": "/static/media/la-boriosa.ee606320.svg"
+  },
+  {
+    "revision": "4eb37a6e57ce2d0f0236a546d590b0e0",
+    "url": "/static/media/linkedinIcon.4eb37a6e.svg"
+  },
+  {
+    "revision": "63f5286d298b856353805574c943c892",
+    "url": "/static/media/mail1.63f5286d.svg"
+  },
+  {
+    "revision": "9cff26f7a1c23fe1843802b960dd9298",
+    "url": "/static/media/navigation.9cff26f7.svg"
+  },
+  {
+    "revision": "b6a8feeb7edaac7e9b3eb9620be84afc",
+    "url": "/static/media/news1.b6a8feeb.svg"
+  },
+  {
+    "revision": "4015e91eb1bec20e30c4829ef49f8b13",
+    "url": "/static/media/parallaxEffect.4015e91e.svg"
+  },
+  {
+    "revision": "2f5bfc3dbbe16677eaec7c84911cefbe",
+    "url": "/static/media/photography1.2f5bfc3d.jpg"
+  },
+  {
+    "revision": "4017765b226a6a711624692a0b29d090",
+    "url": "/static/media/photography2.4017765b.jpg"
+  },
+  {
+    "revision": "f54e1b95aa447474ea20c6dd3030c512",
+    "url": "/static/media/pinterestIcon.f54e1b95.svg"
+  },
+  {
+    "revision": "49864fb4c9bde5f0fec697ca59ea6521",
+    "url": "/static/media/portfolio1.49864fb4.svg"
+  },
+  {
+    "revision": "c3428596069e07474e1be691a5784549",
+    "url": "/static/media/prise1.c3428596.svg"
+  },
+  {
+    "revision": "652aeb246e41d03ddd6be3022edc426e",
+    "url": "/static/media/retro-bike.652aeb24.svg"
+  },
+  {
+    "revision": "e67574b9a0dfffee420a29c4bdc6582a",
+    "url": "/static/media/servise1.e67574b9.svg"
+  },
+  {
+    "revision": "0c4b91842d0828a247234e85ae4abad1",
+    "url": "/static/media/simpleClean.0c4b9184.svg"
+  },
+  {
+    "revision": "cfbffd3cd3731cb9e9a4846666a7c3f7",
+    "url": "/static/media/skypeIcon.cfbffd3c.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ac641483a67063ab204ffb10c86bdd84",
+    "url": "/static/media/star.ac641483.svg"
+  },
+  {
+    "revision": "40ecd007931e9f28885462acb05a7700",
+    "url": "/static/media/support.40ecd007.svg"
+  },
+  {
+    "revision": "3d6fa4c973cc1e3076cb1027d9cc16ea",
+    "url": "/static/media/top-img.3d6fa4c9.jpg"
+  },
+  {
+    "revision": "1d79ec92ba733d90e55e0dd0b7013eb9",
+    "url": "/static/media/tvitterIcon.1d79ec92.svg"
+  },
+  {
+    "revision": "84f776f2691c279ee62dc7b29779a6fe",
+    "url": "/static/media/vintage-bike.84f776f2.svg"
+  },
+  {
+    "revision": "7cb287e255faa6b5de2edd41dd90edb3",
+    "url": "/static/media/vintage-oliva.7cb287e2.svg"
+  }
+]);
